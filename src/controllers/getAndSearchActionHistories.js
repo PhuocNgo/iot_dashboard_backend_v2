391b2727887c7a6bae678c3195e5e1e8e35015d8
PhuocNgo <phuocngo160802@gmail.com>
2024-10-26 00:10:34 +0700

    update 2 search apis

--- src/controllers/getAndSearchActionHistories.js
+++ src/controllers/getAndSearchActionHistories.js
@@ -14,24 +14,65 @@ const getAndSearchActionHistories = (req, res) => {
   const pageSize = parseInt(req.query.page_size);
   const skipPages = (page - 1) * pageSize;
   const sortField = req.query.sort_field;
-  const sortDirection = req.query.sort;
+  const sortDirection = req.query.sort_direction;
+  const speciType = req.body.speci_type;
+
+  const dateRegex =
+    /^(?:\d{2}(\/|-)\d{2}(\/|-)\d{4}|\d{2}(\/|-)\d{4}|\d{2}(\/|-)\d{2})$/;
+  const dateAndTimeRegex =
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?(Z|([+-]\d{2}:\d{2}))?$/;
 
   const bodyReqSearch = req.body.search_value;
-  const query = {};
+  let query = {};
 
   if (devicesList.includes(bodyReqSearch)) {
     query.device_name = bodyReqSearch;
   } else if (actionsList.includes(bodyReqSearch)) {
     query.action = bodyReqSearch;
-  } else if (bodyReqSearch) {
-    const [hour, minute] = bodyReqSearch.split(":");
-    query.time = minute
-      ? {
-          $regex: `^.*T${hour}:${minute}:.*Z$`,
-        }
-      : {
-          $regex: `^.*T${hour}:\\d{2}:.*Z$`,
-        };
+  } else if (dateAndTimeRegex.test(bodyReqSearch)) {
+    query.time = bodyReqSearch;
+  } else if (dateRegex.test(bodyReqSearch) && speciType === "date") {
+    let [day, month, year] = bodyReqSearch.includes("-")
+      ? bodyReqSearch.split("-")
+      : bodyReqSearch.split("/");
+    if (month.length === 4 || day.length === 4) {
+      console.log(`month: ${month}, day: ${day}`);
+      query = {
+        $or: [
+          { time: { $regex: `^${month}-${day}-\\d{2}T.*Z$` } },
+          { time: { $regex: `^${day}-${month}-\\d{2}T.*Z$` } },
+        ],
+      };
+    } else {
+      query = year
+        ? {
+            $or: [
+              { time: { $regex: `^${year}-${month}-${day}T.*Z$` } },
+              { time: { $regex: `^${year}-${day}-${month}T.*Z$` } },
+            ],
+          }
+        : {
+            $or: [
+              { time: { $regex: `^\\d{4}-${month}-${day}T.*Z$` } },
+              { time: { $regex: `^\\d{4}-${day}-${month}T.*Z$` } },
+            ],
+          };
+    }
+  } else if (bodyReqSearch && speciType === "time") {
+    const [hour, minute, second] = bodyReqSearch.split(":");
+    if (second) {
+      query.time = {
+        $regex: `^.*T${hour}:${minute}:${second}.*Z$`,
+      };
+    } else {
+      query.time = minute
+        ? {
+            $regex: `^.*T${hour}:${minute}:.*Z$`,
+          }
+        : {
+            $regex: `^.*T${hour}:\\d{2}:.*Z$`,
+          };
+    }
   }
 
   collection
--- src/controllers/getAndSearchSensorDatas.js
+++ src/controllers/getAndSearchSensorDatas.js
@@ -11,33 +11,90 @@ const getAndSearchSensorDatas = (req, res) => {
   const pageSize = parseInt(req.query.page_size);
   const skipPages = Math.abs(page - 1) * pageSize;
   const sortField = req.query.sort_field;
-  const sortDirection = req.query.sort;
+  const sortDirection = req.query.sort_direction;
   const bodyReq = req.body.search_value;
   const bodyReqSpeciType = req.body.speci_type;
-  const query = {};
+  let query = {};
 
-  switch (bodyReqSpeciType) {
-    case "time":
-      {
-        const [hour, minute] = bodyReq.split(":");
-        query.time = minute
-          ? {
-              $regex: `^.*T${hour}:${minute}:.*Z$`,
-            }
-          : {
-              $regex: `^.*T${hour}:\\d{2}:.*Z$`,
+  const dateRegex =
+    /^(?:\d{2}(\/|-)\d{2}(\/|-)\d{4}|\d{2}(\/|-)\d{4}|\d{2}(\/|-)\d{2})$/;
+  const dateAndTimeRegex =
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?(Z|([+-]\d{2}:\d{2}))?$/;
+
+  if (bodyReq) {
+    if (dateAndTimeRegex.test(bodyReq)) {
+      query.time = bodyReq;
+    }
+    switch (bodyReqSpeciType) {
+      case "date":
+        if (dateRegex.test(bodyReq)) {
+          console.log("bodyReq::", bodyReq);
+          let [day, month, year] = bodyReq.includes("-")
+            ? bodyReq.split("-")
+            : bodyReq.split("/");
+          if (month.length === 4 || day.length === 4) {
+            console.log(`month: ${month}, day: ${day}`);
+            query = {
+              $or: [
+                { time: { $regex: `^${month}-${day}-\\d{2}T.*Z$` } },
+                { time: { $regex: `^${day}-${month}-\\d{2}T.*Z$` } },
+              ],
             };
-      }
-      break;
-    case "temperature":
-      query.temperature = bodyReq;
-      break;
-    case "brightness":
-      query.brightness = bodyReq;
-      break;
-    case "humidity":
-      query.humidity = bodyReq;
-      break;
+          } else {
+            query = year
+              ? {
+                  $or: [
+                    { time: { $regex: `^${year}-${month}-${day}T.*Z$` } },
+                    { time: { $regex: `^${year}-${day}-${month}T.*Z$` } },
+                  ],
+                }
+              : {
+                  $or: [
+                    { time: { $regex: `^\\d{4}-${month}-${day}T.*Z$` } },
+                    { time: { $regex: `^\\d{4}-${day}-${month}T.*Z$` } },
+                  ],
+                };
+          }
+        }
+
+        break;
+      case "time":
+        {
+          const [hour, minute, second] = bodyReq.split(":");
+          if (second) {
+            query.time = {
+              $regex: `^.*T${hour}:${minute}:${second}.*Z$`,
+            };
+          } else {
+            query.time = minute
+              ? {
+                  $regex: `^.*T${hour}:${minute}:.*Z$`,
+                }
+              : {
+                  $regex: `^.*T${hour}:\\d{2}:.*Z$`,
+                };
+          }
+        }
+        break;
+      case "temperature":
+        query.temperature = {
+          $gte: parseFloat(bodyReq),
+          $lte: parseFloat(bodyReq) + 0.9,
+        };
+        break;
+      case "brightness":
+        query.brightness = {
+          $gte: parseFloat(bodyReq),
+          $lte: parseFloat(bodyReq) + 100,
+        };
+        break;
+      case "humidity":
+        query.humidity = {
+          $gte: parseFloat(bodyReq),
+          $lte: parseFloat(bodyReq) + 10,
+        };
+        break;
+    }
   }
 
   collection
